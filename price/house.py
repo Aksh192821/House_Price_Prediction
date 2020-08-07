9578e2d6a0e375288f73efd9dbdbd01982e23bc5
PreetiVerma <preetiverma1028@gmail.com>
2020-08-07 23:52:05 +0530

    finalcommit

--- price/house.py
+++ price/house.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import sklearn
 import seaborn as sns
-import statsmodels.api as sm
+#import statsmodels.api as sm
 import matplotlib.pyplot as plt
 #%matplotlib inline
 import pickle
